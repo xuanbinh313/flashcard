170b99243651fdc9c33aceffa0ffb944a95e8f65
xuanbinh313 <xuanbinh313@gmail.com>
2023-11-27 21:09:41 +0700

    update route

--- deck/urls.py
+++ deck/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import index, DeckListView, deckDetail, play_audio, next_reviewed
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('decks/', DeckListView.as_view(), name='deck_list'),
-    path('decks/<int:id>/', deckDetail, name='deck_detail'),
+    path('dashboard/', index, name='index'),
+    path('', DeckListView.as_view(), name='deck_list'),
+    path('/<int:id>/', deckDetail, name='deck_detail'),
     path('play_audio/<int:card_id>/', play_audio, name='play_audio'),
     path('next_reviewed/<str:deck_id>/<str:status>/', next_reviewed, name='next_reviewed')
 ]
